252cc9b2b5186fd5f449034b36fdaf983e219895
Daphnee-C <dadacc16@gmail.com>
2025-03-05 17:19:42 +0100

    deleting commentary

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  // const [count, setCount] = useState(29);
   const [users, setUsers] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -11,10 +10,6 @@ const App = () => {
   const [firstname, setFirstname] = useState("");
   const [telephone, setTelephone] = useState("");
   const [address, setAddress] = useState("");
-
-  // const myName = "Daphnée";
-
-  // const [displayError, setDisplayError] = useState(false)
 
   const fetchApi = async () => {
     try {
@@ -41,7 +36,6 @@ const App = () => {
 
   useEffect(() => {
     fetchApi();
-    // console.log(users);
   }, []);
 
   if (loading) return <h1>Chargement...</h1>;
@@ -50,10 +44,6 @@ const App = () => {
   return (
     <>
       <h1 className="myH1">Hello world</h1>
-      {/* <p> 
-        Hello my name is {myName} and i am {count}
-      </p> */}
-      {/* <button onClick={() => setCount(count + 1)}>+1</button> */}
       {users &&
         !loading &&
         users.map((user) => {
